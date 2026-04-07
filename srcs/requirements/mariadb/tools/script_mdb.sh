871d4ad168adafe66ebcd6f2a8c51769cfd74857
bedantas <bedantas@bedantas.myguest.virtualbox.org>
2026-04-07 12:22:04 -0300

    new script mariadb

--- srcs/requirements/mariadb/tools/script_mdb.sh
+++ srcs/requirements/mariadb/tools/script_mdb.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# “execute este script usando o interpretador bash”
 
-# dá acesso ao banco, agora mysql é dono dessa pasta
 chown -R mysql:mysql /var/lib/mysql
 
-# inicia o mysql, o & significa: “execute em background”, sem & o script para aqui
+# inicia temporariamente
 mysqld_safe &
 
-# espera o banco subir
 for i in {30..0}; do
     if mysqladmin ping -u root --silent; then
         break
@@ -15,15 +12,20 @@ for i in {30..0}; do
     sleep 1
 done
 
-# só cria se ainda não existir. Usa heredoc -> EOF
-if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-    mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+if [ "$i" = 0 ]; then
+    exit 1
+fi
+
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-fi
 
-wait
+# mata processo temporário
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+
+#sobe corretamente em foreground
+exec mysqld_safe
